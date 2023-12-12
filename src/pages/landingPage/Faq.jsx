ae7a095060c6600f2b9b17bb7f2475b62b3d64bc
DanGoodnews <mac@MACs-MacBook-Pro.local>
2023-12-11 21:02:02 -0800

    Dan

--- src/pages/landingPage/Faq.jsx
+++ src/pages/landingPage/Faq.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import '../../styles/Faq.scss'
-import img from '../../assets/Young.png'
+// import img from '../../assets/Young.png'
+// import fagImg from '../../assets/fagimg.png'
 import up from '../../assets/UP.png'
 const Faq = () => {
   return (
-    <>
-    <div className="Faq">
-     <div className="imgBx">
-        <img src={img} alt="" />
-      </div>
+    <div>
+      <div className="Faq">
+        <div className="imgBx">
+          {/* <img src={fagImg} alt="" /> */}
+        </div>
         <div className="content">
-        <h6>Frequently Asked Questions</h6>
-    <div className="details">
-      <p>How do i set up my account?</p>
-      <p>Who registers the employee?</p>
-      <p>Will the organisation have a special URL ?</p>
-      <p>How many employee can I onboard?</p>
-      <p>Can an admin use one account for different organisation?</p>
-    </div>
+          <h6>Frequently Asked Questions</h6>
+          <div className="details">
+            <p>How do i set up my account?</p>
+            <p>Who registers the employee?</p>
+            <p>Will the organisation have a special URL ?</p>
+            <p>How many employee can I onboard?</p>
+            <p>Can an admin use one account for different organisation?</p>
+          </div>
+        </div>
+
+        <img className='img' src={up} alt="" />
       </div>
-    
-      <img className='img' src={up} alt="" />
     </div>
-    </>
   )
 }
 
